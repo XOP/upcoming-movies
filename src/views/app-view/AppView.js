f37ca8c81fcd9bf1780e31f78e3bfe0742a03bc8
XOP <wdybih@gmail.com>
2021-10-13 21:58:24 +0200

    months display

--- src/views/app-view/AppView.js
+++ src/views/app-view/AppView.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import Heading from "choom/lib/components/heading/Heading";
 import Hold from "choom/lib/components/layout/Hold";
 import Loader from "choom/lib/components/loader/Loader";
 
@@ -27,37 +28,59 @@ const AppView = () => {
   const movieList = useSelector(movieListSelector);
   const appStatus = useSelector(statusSelector);
 
+  let prevMonth = 0;
+
   return (
     <Hold>
-      {(appStatus === STATUS.loading || appStatus === STATUS.init) && <Loader />}
+      {(appStatus === STATUS.loading || appStatus === STATUS.init) && (
+        <Loader />
+      )}
       {appStatus === STATUS.idle &&
         !!movieList.length &&
         movieList.map((item) => {
-          const parsedReleaseDate = new Date(item.releaseDate).toLocaleString('en-GB', {
+          const date = new Date(item.releaseDate);
+          const parsedReleaseDate = date.toLocaleString("en-GB", {
             weekday: undefined,
-            month: 'long',
-            day: '2-digit',
-            year: 'numeric'
+            month: "long",
+            day: "2-digit",
+            year: "numeric",
           });
 
+          const releaseMonth = date.toLocaleString("en-GB", {
+            month: "long",
+          });
+
+          let month = null;
+
+          if (releaseMonth !== prevMonth) {
+            prevMonth = releaseMonth;
+            month = (
+              <Heading as="div" level="3" align="right" mt="2" mb="1">
+                {releaseMonth}
+              </Heading>
+            );
+          }
+
           return (
-            <MovieCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              imgSrc={item.imgSrc}
-              summary={item.summary}
-              releaseDate={parsedReleaseDate}
-              linkTo={{
-                pathname: createRoute(routeNames.ITEM, item.id),
-                state: {
-                  id: item.id,
-                  title: item.title,
-                  releaseDate: parsedReleaseDate,
-                },
-              }}
-              onClick={() => dispatch(modalToggle(true))}
-            />
+            <React.Fragment key={item.id}>
+              {month}
+              <MovieCard
+                id={item.id}
+                title={item.title}
+                imgSrc={item.imgSrc}
+                summary={item.summary}
+                releaseDate={parsedReleaseDate}
+                linkTo={{
+                  pathname: createRoute(routeNames.ITEM, item.id),
+                  state: {
+                    id: item.id,
+                    title: item.title,
+                    releaseDate: parsedReleaseDate,
+                  },
+                }}
+                onClick={() => dispatch(modalToggle(true))}
+              />
+            </React.Fragment>
           );
         })}
     </Hold>
