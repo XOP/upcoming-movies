751e7eef8b9d65d47e38b7f2ea24c427d503cfb8
XOP <wdybih@gmail.com>
2021-10-13 22:18:09 +0200

    logo exchange

--- src/components/features/header/Header.js
+++ src/components/features/header/Header.js
@@ -1,9 +1,8 @@
 import Panel from "choom/lib/components/panel/Panel";
 import Heading from "choom/lib/components/heading/Heading";
-import Icon from "choom/lib/components/icon/Icon";
 import Media from "choom/lib/components/layout/Media";
 
-import { Video } from "react-feather";
+import logoSrc from "../../../assets/images/coming_up.png";
 
 import styles from "./Header.module.css";
 
@@ -11,10 +10,9 @@ const Header = () => {
   return (
     <Panel as="header" padding="1.5" className={styles.root} placement="top">
       <Media
+        align='center'
         start={
-          <Icon size="big">
-            <Video />
-          </Icon>
+          <img src={logoSrc} className={styles.logo} alt="Coming up next logo" /> 
         }
       >
         <Heading level="4" as="h1" colorInherit>
